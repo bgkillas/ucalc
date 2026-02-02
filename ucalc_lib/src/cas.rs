da1717b2fb886a35a59406933cbeaede6abd0592
bgkillas <walhelm@proton.me>
2026-02-02 17:52:21 -0500

    simplify cas

--- ucalc_lib/src/cas.rs
+++ ucalc_lib/src/cas.rs
@@ -12,22 +12,21 @@ impl TokensRef<'_> {
     ) -> Complex {
         let mut i = self.len();
         let mut ret = Complex::from(0);
-        let mut stack = Tokens(Vec::with_capacity(self.len()));
         let mut inner_stack = Tokens(Vec::with_capacity(self.len()));
         while i > 1 {
             i -= 1;
             match self[i] {
                 Token::Operator(operator) if let Some(inv) = operator.inverse() => {
-                    stack.extend((0..operator.inputs() - 1).map(|_| Token::Skip(0)));
-                    for k in 0..operator.inputs() - 1 {
+                    if operator.inputs() == 2 {
                         let last = TokensRef(&self[..i]).get_last(funs);
-                        stack[operator.inputs() - (k + 2)] = TokensRef(&self[last..i])
+                        let num = TokensRef(&self[last..i])
                             .compute_buffer_with(fun_vars, vars, funs, &mut inner_stack, offset)
                             .into();
                         i = last;
+                        inv.compute_on(&mut ret, &[num]);
+                    } else {
+                        inv.compute_on(&mut ret, &[]);
                     }
-                    inv.compute_on(&mut ret, &stack);
-                    stack.clear();
                 }
                 _ => return Complex::from(Constant::Nan),
             }
